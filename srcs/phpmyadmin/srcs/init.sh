142a8cb28f572a5a4448320291587a3f6275e86a
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-21 19:19:20 +0900

    tmp

--- srcs/phpmyadmin/srcs/init.sh
+++ srcs/phpmyadmin/srcs/init.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# sed -i \
-# 	-e s/';listen.owner = nobody'/'listen.owner = nginx'/g \
-# 	-e s/';listen.group = nobody'/'listen.group = nginx'/g \
-# 	-e s/'user = nobody'/'user = nginx'/g \
-# 	-e s/'group = nobody'/'group = nginx'/g \
-# 	/etc/php7/php-fpm.d/www.conf
-
 php-fpm7
 nginx
 

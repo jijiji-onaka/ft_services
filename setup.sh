d6bd41a88731208383cafafb6793af1d59266ad7
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-21 15:59:16 +0900

    tmp

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ kubectl apply -f ./srcs/metallb-system.yaml
 eval $(minikube docker-env)
 docker build -t tjinichi/nginx:000 ./srcs/nginx
 kubectl apply -f ./srcs/nginx/nginx.yaml
- docker build -t tjinichi/phpmyadmin ./srcs/phpmyadmin
+ docker build -t tjinichi/phpmyadmin:000 ./srcs/phpmyadmin
  kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 # kubectl apply -f ./srcs/nginx/nginx-replicaset.yaml
 

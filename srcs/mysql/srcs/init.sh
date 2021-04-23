f234316812ef7f87a613b9d82561339fbfe62bd6
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-24 02:09:03 +0900

    fix mysql init.sh

--- srcs/mysql/srcs/init.sh
+++ srcs/mysql/srcs/init.sh
@@ -4,17 +4,26 @@ chown -R mysql:mysql /var/lib/mysql
 
 mysql_install_db --user=root --datadir=/var/lib/mysql
 
-mysql_install_db --user=root > /dev/null
+# mysql_install_db --user=root > /dev/null
+
+# cat > /tmp/create_table.sql << EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
+# CREATE DATABASE IF NOT EXISTS wpdb;
+# CREATE USER 'admin'@'%' identified by 'admin';
+# GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
+# FLUSH PRIVILEGES;
+# EOF
 
 cat > /tmp/create_table.sql << EOF
-USE mysql;
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS wpdb;
-CREATE USER 'admin'@'%' identified by 'admin';
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
+DROP DATABASE IF EXISTS test;
 EOF
 
 /usr/bin/mysqld --user=root --bootstrap < /tmp/create_table.sql
+rm -f /tmp/create_table.sql
 
 exec /usr/bin/mysqld --user=root --console

ca3b4940c5e1ddb7bde32286f81ac32af91b9017
tjinichi <tjinichi@student.42tokyo.jp>
2021-04-23 14:54:09 +0200

    fix

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 kubectl delete -f srcs/nginx/nginx.yaml
 kubectl delete -f srcs/phpmyadmin/phpmyadmin.yaml
-kubectl delete -f srcs/phpmyadmin/wordpress.yaml
-kubectl delete -f srcs/phpmyadmin/mysql.yaml
+kubectl delete -f srcs/wordpress/wordpress.yaml
+kubectl delete -f srcs/mysql/mysql.yaml

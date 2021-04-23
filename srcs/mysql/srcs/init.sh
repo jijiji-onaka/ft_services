d95ff8fa58e9aa2339a90d4531533e91d5d1885f
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-24 02:22:29 +0900

    fix mysql init.sh

--- srcs/mysql/srcs/init.sh
+++ srcs/mysql/srcs/init.sh
@@ -4,26 +4,19 @@ chown -R mysql:mysql /var/lib/mysql
 
 mysql_install_db --user=root --datadir=/var/lib/mysql
 
-# mysql_install_db --user=root > /dev/null
-
-# cat > /tmp/create_table.sql << EOF
-# USE mysql;
-# FLUSH PRIVILEGES;
-# CREATE DATABASE IF NOT EXISTS wpdb;
-# CREATE USER 'admin'@'%' identified by 'admin';
-# GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
-# FLUSH PRIVILEGES;
-# EOF
-
 cat > /tmp/create_table.sql << EOF
 FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS wpdb;
-CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 DROP DATABASE IF EXISTS test;
 EOF
 
 /usr/bin/mysqld --user=root --bootstrap < /tmp/create_table.sql
 rm -f /tmp/create_table.sql
 
-exec /usr/bin/mysqld --user=root --console
+# exec /usr/bin/mysqld --user=root --console
+
+/usr/bin/mysqld_safe --user=root
+
+tail -f

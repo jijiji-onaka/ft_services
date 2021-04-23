d60362ee48067c5a7a07c0d61b8fd320824fa0ec
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-23 22:22:20 +0900

    add kubectl get all in clean.sh

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@ kubectl delete -f srcs/nginx/nginx.yaml
 kubectl delete -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl delete -f srcs/wordpress/wordpress.yaml
 kubectl delete -f srcs/mysql/mysql.yaml
+
+
+kubectl get all

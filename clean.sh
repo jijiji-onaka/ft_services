c9fb0677a68f5cafd4ca596a99be1771d162af6d
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-23 22:28:31 +0900

    fix clean.sh

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ kubectl delete -f srcs/nginx/nginx.yaml
 kubectl delete -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl delete -f srcs/wordpress/wordpress.yaml
 kubectl delete -f srcs/mysql/mysql.yaml
+kubectl delete -f ./srcs/metallb-system.yaml
 
 
 kubectl get all

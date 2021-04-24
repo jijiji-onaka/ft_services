c723176877e8673554c14f18eb4ff6b7e023c295
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-24 20:59:20 +0900

    fix phpmyadmin

--- srcs/phpmyadmin/srcs/init.sh
+++ srcs/phpmyadmin/srcs/init.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+sed	-e "s/localhost/mysql/g" \
+	/var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php \
+	> /var/www/localhost/htdocs/phpmyadmin/config.inc.php
+
 cat > /etc/phpmyadmin/config.secret.inc.php <<EOF
 <?php
 \$cfg['blowfish_secret'] = '$(cat /dev/urandom | tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ,.+\-!' | fold -w 32 | grep -E '[12345678]' | grep -E '[,\.+\-\!]' | head -n 1)';
 EOF
+
 
 # mv /etc/phpmyadmin/config.inc.php /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php
 # mv /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php /var/www/localhost/htdocs/phpmyadmin/config.inc.php

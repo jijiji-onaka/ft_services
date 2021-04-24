c43e68690812c078779c2eda164aadcf473d3ae4
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-24 17:24:37 +0900

    fix phpmyadmin

--- srcs/phpmyadmin/srcs/init.sh
+++ srcs/phpmyadmin/srcs/init.sh
@@ -5,6 +5,8 @@ cat > /etc/phpmyadmin/config.secret.inc.php <<EOF
 \$cfg['blowfish_secret'] = '$(cat /dev/urandom | tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ,.+\-!' | fold -w 32 | grep -E '[12345678]' | grep -E '[,\.+\-\!]' | head -n 1)';
 EOF
 
+rm /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php
+
 php-fpm7
 nginx
 

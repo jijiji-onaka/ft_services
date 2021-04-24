c3aad25188166205f58d309fb46fe3e023140029
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-24 20:47:25 +0900

    fix phpmyadmin

--- srcs/mysql/srcs/init.sh
+++ srcs/mysql/srcs/init.sh
@@ -15,13 +15,21 @@ cat > /tmp/create_table.sql << EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS wpdb;
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
+FLUSH PRIVILEGES;
 DROP DATABASE IF EXISTS test;
 EOF
+# GRANT ALL PRIVILEGES ON wpdb.* TO 'admin'@'%';
 
-/usr/bin/mysqld --user=root --bootstrap < /tmp/create_table.sql
+# CREATE USER 'user'@'localhost' IDENTIFIED BY 'your_password';
+# GRANT ALL PRIVILEGES ON *.* To 'user'@'localhost' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+# ALTER USER user IDENTIFIED WITH mysql_native_password BY
+# 'your_password';
+
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql --bootstrap < /tmp/create_table.sql
 rm -f /tmp/create_table.sql
 
-/usr/bin/mysqld_safe --user=root
+/usr/bin/mysqld_safe --user=root --datadir=/var/lib/mysql
 
 tail -f
--- srcs/phpmyadmin/srcs/init.sh
+++ srcs/phpmyadmin/srcs/init.sh
@@ -5,7 +5,8 @@ cat > /etc/phpmyadmin/config.secret.inc.php <<EOF
 \$cfg['blowfish_secret'] = '$(cat /dev/urandom | tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ,.+\-!' | fold -w 32 | grep -E '[12345678]' | grep -E '[,\.+\-\!]' | head -n 1)';
 EOF
 
-rm /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php
+# mv /etc/phpmyadmin/config.inc.php /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php
+# mv /var/www/localhost/htdocs/phpmyadmin/config.sample.inc.php /var/www/localhost/htdocs/phpmyadmin/config.inc.php
 
 php-fpm7
 nginx

578f68aba6cbb8fa2283815ed7f17c6c0628a32d
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-26 19:42:29 +0900

    add grafana

--- srcs/phpmyadmin_copy/srcs/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -u
-
-cat > /etc/phpmyadmin/config.secret.inc.php <<EOF
-<?php
-\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
-EOF
-
-nginx && php-fpm7 && tail -f

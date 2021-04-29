34d4f8e780a566fd0e1d1b1bb86f4fe6841b7704
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-04-29 17:40:23 +0900

    fix grafana: dashboard.yaml

--- tmp.sh
+++ tmp.sh
@@ -19,22 +19,38 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl apply -f ./srcs/metallb-system.yaml
 
 eval $(minikube docker-env)
+echo "${COLOR_MSG} [ docker build ] ${COLOR_RESET}\n"
+echo "${COLOR_MSG} building nginx ... ${COLOR_RESET}\n"
 docker build -t tjinichi/nginx:000 ./srcs/nginx
+echo "${COLOR_MSG} building mysql ... ${COLOR_RESET}\n"
 docker build -t tjinichi/mysql:000 ./srcs/mysql
+echo "${COLOR_MSG} building phpmyadmin ... ${COLOR_RESET}\n"
 docker build -t tjinichi/phpmyadmin:000 ./srcs/phpmyadmin
+echo "${COLOR_MSG} building wordpress ... ${COLOR_RESET}\n"
 docker build -t tjinichi/wordpress:000 ./srcs/wordpress
+echo "${COLOR_MSG} building ftps ... ${COLOR_RESET}\n"
 docker build -t tjinichi/ftps:000 ./srcs/ftps
+echo "${COLOR_MSG} building influxdb ... ${COLOR_RESET}\n"
 docker build -t tjinichi/influxdb:000 ./srcs/influxdb
+echo "${COLOR_MSG} building grafana ... ${COLOR_RESET}\n"
 docker build -t tjinichi/grafana:000 ./srcs/grafana
 
+
+echo "${COLOR_MSG} [ kubectl apply ] ${COLOR_RESET}\n"
+echo "${COLOR_MSG} apply nginx ... ${COLOR_RESET}\n"
 kubectl apply -f ./srcs/nginx/nginx.yaml
+echo "${COLOR_MSG} apply mysql ... ${COLOR_RESET}\n"
 kubectl apply -f ./srcs/mysql/mysql.yaml
+echo "${COLOR_MSG} apply phpmyadmin ... ${COLOR_RESET}\n"
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+echo "${COLOR_MSG} apply wordpress ... ${COLOR_RESET}\n"
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
+echo "${COLOR_MSG} apply ftps ... ${COLOR_RESET}\n"
 kubectl apply -f srcs/ftps/ftps.yaml
+echo "${COLOR_MSG} apply influxdb ... ${COLOR_RESET}\n"
 kubectl apply -f srcs/influxdb/influxdb.yaml
+echo "${COLOR_MSG} apply grafana ... ${COLOR_RESET}\n"
 kubectl apply -f srcs/grafana/grafana.yaml
-# kubectl apply -f ./srcs/nginx/nginx-replicaset.yaml
 
 kubectl get po,svc
 

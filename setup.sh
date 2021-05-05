22b15125d8273da499fcb00fb1af8d596cac6e67
jijiji-onaka <takao-jinjinjin715@i.softbank.jp>
2021-05-05 16:31:30 +0900

    rm wordpress

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ minikube start driver=docker
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml > /dev/null
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml > /dev/null
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
-kubectl apply -f ./srcs/metallb-system.yaml > /dev/null
+kubectl apply -f ./srcs/metallb/metallb-system.yaml > /dev/null
 
 eval $(minikube docker-env)
 echo "${COLOR_MSG} [ docker build ] ${COLOR_RESET}\n"
